3d220a071ee14cf90f7e18293142c12cb0a38fa6
tonkagavin <121142008+tonkagavin@users.noreply.github.com>
2025-11-10 13:03:12 -0500

    Update AboutSection.tsx idols

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -15,7 +15,7 @@ export function AboutSection() {
           <span className="text-[#00ff00]">Location:</span> NC State University | Fayetteville, NC (user's commute location)
         </p>
         <p>
-          <span className="text-[#00ff00]">Idols:</span> My [grand]parents, LeBron, Napoleon, Linus Torvalds
+          <span className="text-[#00ff00]">Idols:</span> My [grand]parents, LeBron, Napoleon, Linus T, Hegel
         </p>
         <p className="mt-4">
           Hello! I'm a passionate software engineer currently studying Computer Science at NC State University. 
